b6a9ad96aa38cd774962f9d2638bf8795ba2c7a5
pmpr.dev <pmpr.dev@gmail.com>
2025-08-11 17:46:15 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             689a2aa54689b             |
+    |             689a2bf3e95f7             |
     |_______________________________________|
 */
  use Pmpr\Module\Panel\Panel; Panel::symcgieuakksimmu();
--- src/Pmpr/Module/Panel/Container.php
+++ src/Pmpr/Module/Panel/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             689a2aa54689b             |
+    |             689a2bf3e95f7             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Panel; use Pmpr\Common\Foundation\Container\Container as BaseClass; abstract class Container extends BaseClass { }
